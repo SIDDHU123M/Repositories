dabecbc6d866046aa1f812151aed5d3df64fa7c0
Sidharth <76509525+SIDDHU123M@users.noreply.github.com>
2024-10-12 20:17:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-let newObj = []
-let div = document.querySelector('#data')
+let newObj = [];
+let div = document.querySelector('#data');
 
 async function getData() {
-    let response = await fetch('ConvertedFiles/united.json')
-    let data = await response.json()
-
-    for (let key in data) {
-        if (data.hasOwnProperty(key)) {
-            // console.log(data[key])
-            data[key].forEach(jsonData => {
-                newObj.push({
-                    title: jsonData.title,
-                    content: jsonData.content
-                })
-            })
+    try {
+        let response = await fetch('ConvertedFiles/united.json');
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
         }
-    }
+        let data = await response.json();
 
-    newObj.forEach(element => {
-        div.innerHTML += `
-        <h1>${element.title}</h1>
-        <div>${element.content}</div>
-        `
-    });
+        for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+                data[key].forEach(jsonData => {
+                    newObj.push({
+                        title: jsonData.title,
+                        content: jsonData.content
+                    });
+                });
+            }
+        }
+
+        newObj.forEach(element => {
+            div.innerHTML += `
+            <h1>${element.title}</h1>
+            <div>${element.content}</div>
+            `;
+        });
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
+        div.innerHTML = '<p>Failed to load data. Please try again later.</p>';
+    }
 }
 
-getData()
+getData();
